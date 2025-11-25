7e12ee6a970f6f79af95dc1787e2999e5f355920
Lenix <lenix01x@gmail.com>
2025-11-25 16:50:42 +0100

    🔥 chore(frontend): remove unused components and data
    - delete friends, gameModes, tasks, header, and constants files
    - these components and data are no longer needed

--- frontend/src/components/tasks.ts
+++
@@ -1,5 +0,0 @@
-export function createTasksContainer(): HTMLDivElement {
-  const tasksContainer = document.createElement('div');
-  tasksContainer.className = 'h-80 w-80 bg-red-500';
-  return tasksContainer;
-}
--- frontend/src/data/constants.ts
+++
@@ -1,2 +0,0 @@
-export const rankedModes: string[] = ['1v1', '2v2', '3v3', '4v4', '5v5'];
-export const privateModes: string[] = ['1v1', '2v2', '3v3', '4v4', '5v5', '6v6', '7v7', '8v8', '9v9', '10v10'];

f84eb178fbbd0e72f8daec992cad2f34a77361ac
lenix <lenix01x@gmail.com>
2025-11-26 15:42:31 +0100

    feat(layout): add root layout element with fixed full-screen styling
    - Create root element with id 'root' appended to body
    - Apply fixed position and full width and height styles
    - Use flexbox to align items at the bottom center
    - Set background color to black for the root container
    - Export root element for use in layout components

---
+++ frontend/src/styles/layouts/root.ts
@@ -0,0 +1,8 @@
+import createElement from '../../functions/createElement'
+
+const root = createElement({
+  parent: 'body',
+  id: 'root',
+  className: 'fixed w-full h-full flex items-end justify-center bg-black'
+})
+export default root

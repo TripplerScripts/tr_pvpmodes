9d2566b80e10ae1ab7058eb76a939a45f684be2b
tripplerscripts <tripplerscripts@github.com>
2025-11-26 01:32:33 -0800

    refactor(client): organize game dashboard and matchmaking callbacks
    - Import open, close, and unfocus functions from external modules
    - Move resourceName declaration and add lastCoords state variable
    - Replace inline function with RegisterNuiCallback for startMatchmaking
    - Add callback acknowledgment for matchmaking and unfocus events
    - Use RegisterCommand to handle dashboard opening and capture player coords
    - Remove redundant notifications and inline implementations

--- backend/client/main.ts
+++ backend/client/main.ts
@@ -1,35 +1,29 @@
+import openClosedWorldGame from './functions/openGame'
+import closeDashboard from './functions/closeGame'
+import unFocus from './functions/unfocus'
+
 const resourceName = GetCurrentResourceName()
-
-const startMatchmaking = () => {
-  exports.qbx_core.Notify('Started')
-  emitNet('tr_competitive:server:startMatchmaking')
-}
-
-const closeDashboard = () => {
-  exports.qbx_core.Notify('closed')
-  SendNuiMessage(JSON.stringify({
-    action: 'close',
-  }))
-  SetNuiFocus(false, false)
-}
+let lastCoords: number[]
 
 onNet('tr_competitive:client:shutDownDashboard', () => {
   closeDashboard()
 })
 
 RegisterNuiCallback('startMatchmaking', (data: Object, callback: Function) => {
-  startMatchmaking()
+  exports.qbx_core.Notify('Started')
+  emitNet('tr_competitive:server:startMatchmaking')
+  callback(true)
 })
 
-function openClosedWorldGame() {
-  SendNuiMessage(JSON.stringify({
-    action: 'open',
-    resource: resourceName
-  }))
-  SetNuiFocus(true, true)
-}
+RegisterNuiCallback('unfocus', (data: Object, callback: Function) => {
+  unFocus(lastCoords)
+  callback(true)
+})
 
 RegisterCommand('ww', () => {
   exports.qbx_core.Notify('Opened')
-  openClosedWorldGame()
+  // this must be above
+  lastCoords = GetEntityCoords(PlayerPedId(), true)
+  // this one
+  openClosedWorldGame(resourceName)
 }, false)

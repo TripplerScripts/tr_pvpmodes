273f07d6d5471ca850df01eb271a68ca3b09e5e5
tripplerscripts <tripplerscripts@github.com>
2025-11-26 03:41:44 -0800

    feat(client): improve player placement with delayed positioning and freezing
    - Create camera handle when placing player in lobby
    - Delay setting player coordinates by 2 seconds for better sync
    - Freeze player entity position after coordinates are set
    - Return camera handle from the function for external use

--- backend/client/functions/placePlayer.ts
+++ backend/client/functions/placePlayer.ts
@@ -2,9 +2,13 @@ import setCoords from '../wrappers/setCoords'
 import { lobby, cam } from '../config/lobby'
 
 export default function placePlayerInLobby() {
-  setCoords(lobby)
-  exports.tr_kit.createCam({
+  const handle = exports.tr_kit.createCam({
     coords: cam,
     rotation: { vertical: 0.0, horizontal: 0.0 },
   })
+  setTimeout(() => {
+    setCoords(lobby)
+    FreezeEntityPosition(PlayerPedId(), true)
+  }, 2000)
+  return handle
 }

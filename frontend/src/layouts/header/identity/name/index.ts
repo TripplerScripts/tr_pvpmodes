7a44241da7dd8a8fe71bb1a1a11b7d6bcc21afc8
lenix <lenix01x@gmail.com>
2025-11-28 17:30:18 +0100

    refactor(header): remove unused server name element creation
    - Deleted code that created and inserted 'server-name' element in header
    - Removed setting innerHTML for the server name element
    - Cleaned up redundant element creation to simplify header layout logic

--- frontend/src/layouts/header/identity/name/index.ts
+++
@@ -1,9 +0,0 @@
-import createElement from '../../../../modules/createElement'
-
-createElement({
-  parent: 'identity',
-  id: 'server-name',
-})
-
-const nameElement = document.getElementById('server-name')
-nameElement.innerHTML = 'Trippler'

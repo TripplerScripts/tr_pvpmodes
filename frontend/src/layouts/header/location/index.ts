25ea3144ae7540b461a5f691203f438a969b68e5
lenix <lenix01x@gmail.com>
2025-11-28 22:01:45 +0100

    chore(header): remove unused location text element
    - Deleted createElement call for location text element
    - Removed element reference and innerHTML assignment
    - Cleaned up redundant code in location index file

--- frontend/src/layouts/header/location/index.ts
+++
@@ -1,9 +0,0 @@
-import createElement from '../../../modules/createElement'
-
-createElement({
-  parent: 'location',
-  id: 'location-text',
-})
-
-const locationText = document.getElementById('location-text')
-locationText.innerHTML = 'The Bunker'

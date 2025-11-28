4d2a8fdbb4622637853cd791b25c984f67061f6d
lenix <lenix01x@gmail.com>
2025-11-28 17:29:53 +0100

    feat(header): update server identity display with logo and name
    - Change identity container from 'header' to 'server-identity'
    - Replace 'identity' element with 'server-logo' element
    - Add an image element with id 'server-logo-image' inside 'server-logo'
    - Create new 'server-name' element under 'server-identity'
    - Set inner text of 'server-name' to 'Trippler Scripts'

--- frontend/src/layouts/header/identity/index.ts
+++ frontend/src/layouts/header/identity/index.ts
@@ -1,6 +1,17 @@
 import createElement from '../../../modules/createElement'
 
 createElement({
-  parent: 'header',
-  id: 'identity',
+  parent: 'server-identity',
+  id: 'server-logo',
 })
+
+const logo = document.getElementById('server-logo')
+logo.innerHTML = `<img id='server-logo-image' src='https://r2.fivemanage.com/COKMc8Wcmk9K5dp547rEw/trippler.png'>`
+
+createElement({
+  parent: 'server-identity',
+  id: 'server-name',
+})
+
+const name = document.getElementById('server-name')
+name.innerHTML = 'Trippler Scripts'

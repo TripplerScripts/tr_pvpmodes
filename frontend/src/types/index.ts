7198fcafd2e15568976aec800a5102691b4cc09e
lenix <lenix01x@gmail.com>
2025-11-29 15:54:44 +0100

    refactor(types): make ButtonConfig id property optional
    - Changed ButtonConfig interface id property from required to optional
    - Allows usage of ButtonConfig without specifying an id
    - Improves flexibility in button configuration objects
    - No impact on existing onClick and onHover optional functions

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -3,7 +3,7 @@ import { types } from '../styles/button'
 export interface ButtonConfig {
   parent: string
   content: string
-  id: string
+  id?: string
   type: keyof typeof types
   onClick?: () => void
   onHover?: () => void

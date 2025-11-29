7060bc5cac3df86e4d512d35c135b03d037c460a
lenix <lenix01x@gmail.com>
2025-11-29 19:54:30 +0100

    feat(playerFriends): add initial friends items list
    - Created a new exported array friendsItems with sample friend objects
    - Each friend object includes avatar URL and name properties
    - Added two example friends with identical data for initial usage

---
+++ frontend/src/modules/playerFriends.ts
@@ -0,0 +1,10 @@
+export const friendsItems = [
+  {
+    avatar: 'https://r2.fivemanage.com/COKMc8Wcmk9K5dp547rEw/Lenix_924.png',
+    name: 'Lenix'
+  },
+  {
+    avatar: 'https://r2.fivemanage.com/COKMc8Wcmk9K5dp547rEw/Lenix_924.png',
+    name: 'Lenix'
+  }
+]

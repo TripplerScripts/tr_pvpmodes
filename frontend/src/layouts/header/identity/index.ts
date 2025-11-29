cba2fa7188fe64207812592546bbf1cf958bc584
lenix <lenix01x@gmail.com>
2025-11-29 15:53:59 +0100

    refactor(header): simplify server identity element creation
    - Move server logo image insertion into createElement content
    - Move server name text insertion into createElement content
    - Remove redundant DOM queries and innerHTML assignments
    - Clean up identity component initialization code

--- frontend/src/layouts/header/identity/index.ts
+++ frontend/src/layouts/header/identity/index.ts
@@ -3,15 +3,11 @@ import createElement from '../../../modules/createElement'
 createElement({
   parent: 'server-identity',
   id: 'server-logo',
+  content: `<img id='server-logo-image' src='https://r2.fivemanage.com/COKMc8Wcmk9K5dp547rEw/trippler.png'>`
 })
-
-const logo = document.getElementById('server-logo')
-logo.innerHTML = `<img id='server-logo-image' src='https://r2.fivemanage.com/COKMc8Wcmk9K5dp547rEw/trippler.png'>`
 
 createElement({
   parent: 'server-identity',
   id: 'server-name',
+  content: 'Trippler Scripts'
 })
-
-const name = document.getElementById('server-name')
-name.innerHTML = 'Trippler Scripts'

145cb10353060b614b23890c1cad95db0cc643fe
lenix <lenix01x@gmail.com>
2025-11-29 22:38:04 +0100

    refactor(sendCallback): remove unused sendCallback function
    - Deleted sendCallback function from sendCallback.ts file
    - Removed unnecessary fetch POST request implementation
    - Cleaned up frontend module to reduce dead code

--- frontend/src/modules/sendCallback.ts
+++
@@ -1,9 +0,0 @@
-export default function sendCallback (resourceName: string, callback: string) {
-  fetch(`https://${resourceName}/${callback}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: JSON.stringify({}),
-  })
-}

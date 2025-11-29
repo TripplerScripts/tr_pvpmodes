e9dfc506e4380551213525a88fda3aa6ab7ef722
lenix <lenix01x@gmail.com>
2025-11-29 19:53:39 +0100

    refactor(layouts): replace ids with classNames in header elements
    - Changed 'header' element from id to className
    - Updated 'server-identity' element to use className instead of id
    - Replaced 'player-details' id with className and removed its child elements
    - Simplified header structure by grouping player details under one element with className
    - Improved consistency and styling flexibility by using class names over ids

--- frontend/src/layouts/header/index.ts
+++ frontend/src/layouts/header/index.ts
@@ -2,35 +2,15 @@ import createElement from '../../modules/createElement'
 
 createElement({
   parent: 'root',
-  id: 'header',
+  className: 'header',
 })
 
 createElement({
   parent: 'header',
-  id: 'server-identity',
+  className: 'server-identity',
 })
 
 createElement({
   parent: 'header',
-  id: 'player-details',
-})
-
-createElement({
-  parent: 'player-details',
-  id: 'player-card'
-})
-
-createElement({
-  parent: 'player-details',
-  id: 'player-coins'
-})
-
-createElement({
-  parent: 'player-details',
-  id: 'player-money'
-})
-
-createElement({
-  parent: 'player-details',
-  id: 'player-friends'
+  className: 'player-details',
 })

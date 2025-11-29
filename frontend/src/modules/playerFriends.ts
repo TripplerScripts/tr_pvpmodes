26cfcb60fa62b6a500178ab84e39b1fd17879f7e
lenix <lenix01x@gmail.com>
2025-11-29 21:40:45 +0100

    feat(playerFriends): dynamically update friend list and count every 10 seconds
    - Replace static friends array with interval-based friend element updates
    - Increment friends count on each update cycle
    - Update DOM element with current friends count and icon
    - Import showFriendElement function from right-sidebar layout module
    - Schedule friend element updates every 10 seconds using setInterval

--- frontend/src/modules/playerFriends.ts
+++ frontend/src/modules/playerFriends.ts
@@ -1,10 +1,12 @@
-export const friendsItems = [
-  {
-    avatar: 'https://r2.fivemanage.com/COKMc8Wcmk9K5dp547rEw/Lenix_924.png',
-    name: 'Lenix'
-  },
-  {
-    avatar: 'https://r2.fivemanage.com/COKMc8Wcmk9K5dp547rEw/Lenix_924.png',
-    name: 'Lenix'
+import showFriendElement from '../layouts/body/right-sidebar/index'
+
+let friendsCount = 0
+
+setInterval(() => {
+  showFriendElement('https://r2.fivemanage.com/COKMc8Wcmk9K5dp547rEw/Lenix_924.png', 'Lenix')
+  friendsCount++
+  const countElement = document.getElementById('friends-counts')
+  if (countElement) {
+    countElement.innerHTML = `<i class="fas fa-users"></i> ${friendsCount}`
   }
-]
+}, 10000)

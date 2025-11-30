8fce4aee72db3a7119e68ccbbda01f741dee58d8
lenix <lenix01x@gmail.com>
2025-11-30 19:50:02 +0100

    refactor(styles): remove unused headerStyles and style application code
    - Deleted empty headerStyles object
    - Removed logic that applied styles to elements by class name
    - Cleaned up unused style-related code in header index file

--- frontend/src/styles/header/index.ts
+++
@@ -1,9 +0,0 @@
-const headerStyles = {
-}
-
-Object.entries(headerStyles).forEach(([className, styles]) => {
-  const elements = document.querySelectorAll(`.${className}`)
-  elements.forEach((element) => {
-    (element as HTMLElement).className = styles
-  })
-})

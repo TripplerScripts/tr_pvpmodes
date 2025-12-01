5efed1ec025640fcd47a0b2d2aada35448436ff6
lenix <lenix01x@gmail.com>
2025-12-01 23:22:32 +0100

    feat(invites): add invites container element to the body
    - Created a new 'invites' div inside the body
    - Assigned styling classes for width, height, background, and flex layout
    - Positioned element to hold invite-related content with proper spacing and alignment

--- frontend/src/elements/body/invites/index.ts
+++ frontend/src/elements/body/invites/index.ts
@@ -2,6 +2,12 @@ import Button from "../../../components/button"
 import createElement from "../../../components/createElement"
 
 createElement({
+  parent: "body",
+  id: "invites",
+  className: "w-[65%] h-[75%] bg-gray-300 flex justify-between items-center gap-[25%]"
+})
+
+createElement({
   parent: "invites",
   id: "left-InviteSide",
   className: "w-full flex justify-around"

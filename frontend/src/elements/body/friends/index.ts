20ef94f2b99f227f1722050c8ddeda3dd6235b8a
lenix <lenix01x@gmail.com>
2025-12-01 12:02:41 +0100

    style(friends): update friends panel layout and button styles
    - Reduce friends container width from 25% to 10%
    - Adjust friendsPanel to add overflow auto and remove scrollbar width
    - Remove gap-2 and padding from friendsPanel for cleaner layout
    - Replace default buttons with a styled button containing image and text
    - Add hover effects and flex layout to the new button styling

--- frontend/src/elements/body/friends/index.ts
+++ frontend/src/elements/body/friends/index.ts
@@ -4,23 +4,20 @@ import Button from "../../../components/button"
 createElement({
   parent: "body",
   id: "friends",
-  className: "w-[25%] h-full bg-gray-300 relative overflow-hidden"
+  className: "w-[10%] h-full bg-gray-300 relative overflow-hidden"
 })
 
 createElement({
   parent: "friends",
   id: "friendsPanel",
-  className: "w-full h-full flex flex-col gap-2 justify-start p-5 bg-red-500 absolute right-[-80%] hover:right-[0] transition-all"
+  className: "w-full overflow-auto [scrollbar-width:none] h-full flex flex-col justify-start bg-red-500 absolute right-[-80%] hover:right-[0] transition-all"
 })
 
 Button({
   parent: "friendsPanel",
-  content: "Button",
-  type: "soft"
-})
-
-Button({
-  parent: "friendsPanel",
-  content: "Button",
-  type: "soft"
+  content: `
+    <img class='w-[30%] rounded-md' src='https://r2.fivemanage.com/COKMc8Wcmk9K5dp547rEw/Lenix_924.png'>
+    <span>Lenix</span>
+  `,
+  className: "w-full h-fit p-2 bg-stone-500 flex justify-start items-center gap-2 hover:bg-stone-400 cursor-default transition-all",
 })

e2da2b7857780b93633508b904a16506991d92e0
tripplerscripts <tripplerscripts@github.com>
2025-12-02 07:52:17 -0800

    style(player): remove unnecessary background color from player details container
    - Removed bg-gray-700 class from player details element
    - Cleaned up player container styling for a simpler appearance

--- frontend/src/elements/header/player/index.ts
+++ frontend/src/elements/header/player/index.ts
@@ -3,5 +3,5 @@ import createElement from "../../../components/createElement"
 createElement({
   parent: "header",
   id: "player-details",
-  className: "w-[25%] h-[75%] bg-gray-700 flex items-center justify-around"
+  className: "w-[25%] h-[75%] flex items-center justify-around"
 })

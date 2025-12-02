c25c1de7ff4b9840208c657b6c9e615352914701
tripplerscripts <tripplerscripts@github.com>
2025-12-02 07:47:28 -0800

    feat(elements): add openDashboard initialization and user creation
    - Import initOnBoarding and createUser modules for onboarding process
    - Create div element with id 'root' and classes 'select-none hidden'
    - Append the div element to document body
    - Implement openDashboard function that triggers onboarding if user does not exist
    - Remove 'hidden' class to display the dashboard container upon opening
    - Export openDashboard as default async function

--- frontend/src/elements/index.ts
+++ frontend/src/elements/index.ts
@@ -1,2 +1,16 @@
-const div = document.createElement("div"); div.setAttribute('id', 'root'); div.setAttribute('class', 'select-none')
+import initOnBoarding from '../modules/onBoard'
+import createUser from '../modules/createUser'
+import { resourceName } from '../index'
+
+const div = document.createElement("div")
+div.id = 'root'
+div.className = 'select-none hidden'
 document.body.appendChild(div)
+
+export default async function openDashboard(exist: boolean) {
+  if (div && !exist) {
+    const username: string = await initOnBoarding()
+    createUser(resourceName, username)
+  }
+  div.classList.remove('hidden')
+}

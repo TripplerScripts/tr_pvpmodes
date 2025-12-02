1c83a52572c1a93af2403b90d195bfeb2ecea45f
lenix <lenix01x@gmail.com>
2025-12-02 10:36:21 +0100

    fix(friends): correct panel hidden position for smoother hover transition
    - Added overflow-hidden to friends container to prevent overflow issues
    - Adjusted friendsPanel initial right position from -00% to -80%
    - Ensured smooth slide-in effect on hover from the right side

--- frontend/src/elements/body/friends/index.ts
+++ frontend/src/elements/body/friends/index.ts
@@ -3,11 +3,11 @@ import createElement from "../../../components/createElement"
 createElement({
   parent: "body",
   id: "friends",
-  className: "w-[15%] h-full bg-gray-300 relative"
+  className: "w-[15%] h-full bg-gray-300 relative overflow-hidden "
 })
 
 createElement({
   parent: "friends",
   id: "friendsPanel",
-  className: "w-full overflow-hidden bg-black relative h-full absolute right-[-00%] hover:right-[0] transition-all"
+  className: "w-full overflow-hidden bg-black relative h-full absolute right-[-80%] hover:right-[0] transition-all"
 })

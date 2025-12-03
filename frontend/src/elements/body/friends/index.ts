146bbf2b0419a229bfb325fa76fdc10a52bcd516
tripplerscripts <tripplerscripts@github.com>
2025-12-03 07:46:57 -0800

    fix(friends): remove trailing space and add blur effect to panel
    - Removed trailing space in friends container className
    - Added 'blured-15' class to friendsPanel for blurred background effect
    - Fixed code formatting by closing object literals properly

--- frontend/src/elements/body/friends/index.ts
+++ frontend/src/elements/body/friends/index.ts
@@ -3,11 +3,11 @@ import createElement from "../../../components/createElement"
 createElement({
   parent: "body",
   id: "friends",
-  className: "w-[15%] h-full relative overflow-hidden "
+  className: "w-[15%] h-full relative overflow-hidden"
 })
 
 createElement({
   parent: "friends",
   id: "friendsPanel",
-  className: "w-full overflow-hidden bg-black relative h-full absolute right-[-80%] hover:right-[0] transition-all"
+  className: "w-full overflow-hidden blured-15 relative h-full absolute right-[-80%] hover:right-[0] transition-all"
 })

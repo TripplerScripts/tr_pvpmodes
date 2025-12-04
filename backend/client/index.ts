a32770926f7fefb7a867f6fee781c441f54c2e5e
tripplerscripts <tripplerscripts@github.com>
2025-12-04 14:17:47 -0800

    feat(client): add friend management and matchmaking NUI callbacks
    - Imported lib wrapper for callback handling
    - Added getFriendablePlayers callback to fetch friendable players list
    - Implemented sendUserFriendInvitation callback to send friend invites
    - Added getPlayerFriends callback to retrieve current friends list
    - Added getIncomingFriends and getOutgoingFriends callbacks for friend requests
    - Created callbacks to cancel outgoing friendship and remove incoming requests
    - Added acceptFriendship callback to accept friend requests
    - Updated startMatchmaking callback to notify and emit matchmaking start event

--- backend/client/index.ts
+++ backend/client/index.ts
@@ -1,6 +1,7 @@
 import openClosedWorldGame from './functions/openGame'
 import closeDashboard from './functions/closeGame'
 import unFocus from './functions/unfocus'
+import { lib } from './wrappers/lib'
 
 let lastCoords: number[]
 let camHandle: number
@@ -9,7 +10,48 @@ onNet('tr_competitive:client:shutDownDashboard', () => {
   closeDashboard()
 })
 
-RegisterNuiCallback('startMatchmaking', (data: Object, callback: Function) => {
+RegisterNuiCallback('getFriendablePlayers', async (data: null, callback: Function) => {
+  const players = await lib.callback.await('getFriendablePlayers')
+  callback(players)
+})
+
+RegisterNuiCallback('sendUserFriendInvitation', async (data: Array<string>, callback: Function): Promise<void> => {
+  const response = await lib.callback.await('sendUserFriendInvitation', null, data[0])
+  callback(response)
+})
+
+RegisterNuiCallback('getPlayerFriends', async (data: Array<string>, callback: Function): Promise<void> => {
+  const response = await lib.callback.await('getPlayerFriends')
+  callback(response)
+})
+
+RegisterNuiCallback('getIncomingFriends', async (data: Array<string>, callback: Function): Promise<void> => {
+  const response = await lib.callback.await('getIncomingFriends')
+  callback(response)
+})
+
+RegisterNuiCallback('getOutgoingFriends', async (data: Array<string>, callback: Function): Promise<void> => {
+  const response = await lib.callback.await('getOutgoingFriends')
+  callback(response)
+})
+
+RegisterNuiCallback('cancelOutgoingFriendship', async (data: Array<string>, callback: Function): Promise<void> => {
+  lib.callback.await('cancelOutgoingFriendship', null, data[0])
+})
+
+RegisterNuiCallback('removeIncomingRequest', async (data: Array<string>, callback: Function): Promise<void> => {
+  lib.callback.await('removeIncomingRequest', null, data[0])
+})
+
+RegisterNuiCallback('acceptFriendship', async (data: Array<string>, callback: Function): Promise<void> => {
+  lib.callback.await('acceptFriendship', null, data[0])
+})
+
+RegisterNuiCallback('removeIncomingRequest', async (data: Array<string>, callback: Function): Promise<void> => {
+  lib.callback.await('removeIncomingRequest', null, data[0])
+})
+
+RegisterNuiCallback('startMatchmaking', (data: null, callback: Function) => {
   exports.qbx_core.Notify('Started')
   emitNet('tr_competitive:server:startMatchmaking')
   callback(true)

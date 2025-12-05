84226ba791243c241e1a9af6ddb46626535b2002
tripplerscripts <tripplerscripts@github.com>
2025-12-05 14:26:49 -0800

    refactor(modules): remove unused removePlayerFriendship and serviceUnavailable functions
    - Deleted removePlayerFriendship module and its sendCallback import
    - Deleted serviceUnavailable module and its Alert import
    - Cleaned up unused code to simplify the codebase

--- frontend/src/modules/serviceUnavailable.ts
+++
@@ -1,5 +0,0 @@
-import Alert from "../components/alert"
-
-export default function unavailableNotice() {
-  Alert("Notice", "This feature is still not available yet", "Come back later", () => {})
-}

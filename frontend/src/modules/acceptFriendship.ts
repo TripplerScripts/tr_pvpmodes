5f925106fc41f721e13fdf2a56056380e80f0ea2
tripplerscripts <tripplerscripts@github.com>
2025-12-05 14:11:41 -0800

    refactor(friendship): remove acceptFriendship module and related calls
    - Deleted acceptFriendship function from frontend modules
    - Removed imports of sendCallback and createFriendItem in acceptFriendship
    - Simplified codebase by eliminating unused friendship acceptance logic

--- frontend/src/modules/acceptFriendship.ts
+++
@@ -1,7 +0,0 @@
-import sendCallback from "../components/sendCallback"
-import createFriendItem from "./createFriendItem"
-
-export default function acceptFriendship(name: string) {
-  sendCallback('acceptFriendship', name)
-  createFriendItem(name)
-}

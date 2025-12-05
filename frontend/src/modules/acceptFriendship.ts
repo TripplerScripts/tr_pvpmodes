b2d0ccce3646574ebf7e36d52bec17f316ac266b
tripplerscripts <tripplerscripts@github.com>
2025-12-05 12:56:22 -0800

    feat(friendship): create friend item after accepting friendship
    - Import createFriendItem module in acceptFriendship
    - Call createFriendItem after sending acceptFriendship callback
    - Ensure friend item is added upon accepting friendship request

--- frontend/src/modules/acceptFriendship.ts
+++ frontend/src/modules/acceptFriendship.ts
@@ -1,5 +1,7 @@
 import sendCallback from "../components/sendCallback"
+import createFriendItem from "./createFriendItem"
 
 export default function acceptFriendship(name: string) {
   sendCallback('acceptFriendship', name)
+  createFriendItem(name)
 }

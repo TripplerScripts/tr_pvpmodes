569574e260938e0007d954a145c0360481da75d3
tripplerscripts <tripplerscripts@github.com>
2025-12-05 14:06:29 -0800

    feat(ui): add toggle functionality for friends panel
    - Implement toggleFriendsPanel function to open and close the panel
    - Use CSS classes to shift panel position on toggle
    - Manage panel state with a boolean variable friendsPanelCollapsed
    - Ensure smooth panel transition by adding and removing classes accordingly

---
+++ frontend/src/interactions/toggleFriendsPanel.ts
@@ -0,0 +1,14 @@
+let friendsPanelCollapsed = true
+
+export default function toggleFriendsPanel() {
+  const friendsPanel = document.getElementById('friendsPanel')
+  if (friendsPanelCollapsed) {
+    friendsPanel.classList.remove('right-[-80%]')
+    friendsPanel.classList.add('right-[0]')
+    friendsPanelCollapsed = false
+  } else {
+    friendsPanel.classList.remove('right-[0]')
+    friendsPanel.classList.add('right-[-80%]')
+    friendsPanelCollapsed = true
+  }
+}

55b4b6ed3bca264cabcdabc900d62d07898acb8d
tripplerscripts <tripplerscripts@github.com>
2025-12-05 06:10:56 -0800

    style(player): increase player details width from 25% to 40%
    - Modified the className in player-details element to change width
    - Updated layout proportion for better spacing in header component
    - Maintained height and flex properties unchanged

--- frontend/src/elements/header/player/index.ts
+++ frontend/src/elements/header/player/index.ts
@@ -3,5 +3,5 @@ import createElement from "../../../components/createElement"
 createElement({
   parent: "header",
   id: "player-details",
-  className: "w-[25%] h-[75%] flex items-center justify-around"
+  className: "w-[40%] h-[75%] flex items-center justify-around"
 })

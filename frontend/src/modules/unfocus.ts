802eecfa4ec8240f39801f0b49d397ef2bd76722
tripplerscripts <tripplerscripts@github.com>
2025-12-05 14:26:14 -0800

    refactor(unfocus): remove unfocus module and disable related UI hiding
    - Deleted unfocus.ts module entirely
    - Removed logic for sending unfocus callback
    - Eliminated code that adds 'hidden' class to root element to hide UI

--- frontend/src/modules/unfocus.ts
+++
@@ -1,9 +0,0 @@
-import sendCallback from "../components/sendCallback"
-
-export default function unfocus () {
-  sendCallback('unfocus')
-  const root = document.getElementById('root')
-  if (root) {
-    root.classList.add('hidden')
-  }
-}

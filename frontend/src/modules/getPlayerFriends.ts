330132f5fb44dd5211043c4adcccdcfa311f6a7e
tripplerscripts <tripplerscripts@github.com>
2025-12-05 14:15:55 -0800

    refactor(getPlayerFriends): remove deprecated getPlayerFriends module
    - Deleted unused getPlayerFriends function from the codebase
    - Removed related imports to refreshFriends and sendCallback
    - Simplified friend management by consolidating functionality elsewhere

--- frontend/src/modules/getPlayerFriends.ts
+++
@@ -1,8 +0,0 @@
-import refreshFriends from "../modules/refreshFriends"
-import sendCallback from "../components/sendCallback"
-
-export default async function getPlayerFriends() {
-  const friends = await sendCallback('getPlayerFriends')
-  refreshFriends(friends.length)
-  return friends
-}

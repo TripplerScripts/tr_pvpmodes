9b9fcf6a86f7275e3dabf472eac23bef78946b05
LenixDev <149199196+LenixDev@users.noreply.github.com>
2025-12-06 22:00:59 +0100

    refactor(APIs): simplify getIncomingFriends function
    - Remove explicit async function declaration
    - Return sendCallback promise directly
    - Use generic type parameter for sendCallback call

--- frontend/src/APIs/getIncomingFriends.ts
+++ frontend/src/APIs/getIncomingFriends.ts
@@ -1,6 +1,3 @@
 import sendCallback from "../components/sendCallback"
 
-export default async function getIncomingFriends(): Promise<string[]> {
-  const incomingFriends = await sendCallback('getIncomingFriends')
-  return incomingFriends
-}
+export default () => sendCallback<string[]>('getIncomingFriends')

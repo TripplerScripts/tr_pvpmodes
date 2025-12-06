63a8199a1ea2d818f41b67dc5e917f3ed5bc9947
LenixDev <149199196+LenixDev@users.noreply.github.com>
2025-12-06 22:00:03 +0100

    fix(server): make doesUserAlreadyExist callback asynchronous
    - Changed callback registration to async function
    - Added await to doesUserAlreadyExist call
    - Ensured proper handling of asynchronous operation in callback

--- backend/server/APIs/modules/doesUserAlreadyExist.ts
+++ backend/server/APIs/modules/doesUserAlreadyExist.ts
@@ -2,7 +2,7 @@ import lib from '../../lib'
 import doesUserAlreadyExist from '../../modules/doesUserExist'
 
 export default (): void => {
-  lib.callback.register('doesUserAlreadyExist', (source: string) => {
-    return doesUserAlreadyExist(source)
+  lib.callback.register('doesUserAlreadyExist', async (source: string) => {
+    return await doesUserAlreadyExist(source)
   })
 }

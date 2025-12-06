f9bb3b77ce4d9f29ed0697c503e6fb73d79a5c93
LenixDev <149199196+LenixDev@users.noreply.github.com>
2025-12-06 22:33:15 +0100

    refactor(getIncomingFriends): simplify callback registration and use utility for license
    - Replace inline GetPlayerIdentifierByType call with getPlayerLicense utility
    - Streamline export default function to directly return the callback registration
    - Remove unnecessary void return type annotation
    - Maintain existing functionality to fetch and parse incoming invitations data

--- backend/server/APIs/modules/getIncomingFriends.ts
+++ backend/server/APIs/modules/getIncomingFriends.ts
@@ -1,10 +1,8 @@
 import lib from '../../lib'
 import { getSingleRow } from '../../database'
+import getPlayerLicense from '../../utils/getPlayerLicense'
 
-export default (): void => {
-  lib.callback.register('getIncomingFriends', async (source: string) => {
-    const license = GetPlayerIdentifierByType(source, 'license')
-    const response = await getSingleRow(['incomingInvitations'], 'tr_competitive_users', 'license', license)
-    return response ? JSON.parse(response.incomingInvitations) : []
-  })
-}
+export default () => lib.callback.register('getIncomingFriends', async (source: string) => {
+  const response = await getSingleRow(['incomingInvitations'], 'tr_competitive_users', 'license', getPlayerLicense(source))
+  return response ? JSON.parse(response.incomingInvitations) : []
+})

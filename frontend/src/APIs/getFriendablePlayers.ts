dc6c6477d7ac8fa4fd1307bfe70d274f636796c6
LenixDev <149199196+LenixDev@users.noreply.github.com>
2025-12-06 22:00:46 +0100

    refactor(APIs): simplify getFriendablePlayers function implementation
    - Removed unused GetFriendablePlayersResponse import
    - Changed return type from Promise<GetFriendablePlayersResponse> to Promise<string[]>
    - Refactored function to directly return sendCallback call without explicit return statement or type annotation

--- frontend/src/APIs/getFriendablePlayers.ts
+++ frontend/src/APIs/getFriendablePlayers.ts
@@ -1,6 +1,3 @@
 import sendCallback from "../components/sendCallback"
-import { GetFriendablePlayersResponse } from "../types"
 
-export default (): Promise<GetFriendablePlayersResponse> => {
-  return sendCallback('getFriendablePlayers')
-}
+export default () => sendCallback<string[]>('getFriendablePlayers')

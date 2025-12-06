8e76c9b1da9e0b7d9692c90f3bb15a7f4c7bd3a9
LenixDev <149199196+LenixDev@users.noreply.github.com>
2025-12-06 22:33:08 +0100

    refactor(APIs): simplify doesUserAlreadyExist module export
    - Remove unnecessary function wrapper around the register call
    - Directly export the callback registration expression
    - Improve code conciseness and readability in the module

--- backend/server/APIs/modules/doesUserAlreadyExist.ts
+++ backend/server/APIs/modules/doesUserAlreadyExist.ts
@@ -1,8 +1,4 @@
 import lib from '../../lib'
 import doesUserAlreadyExist from '../../modules/doesUserExist'
 
-export default (): void => {
-  lib.callback.register('doesUserAlreadyExist', async (source: string) => {
-    return await doesUserAlreadyExist(source)
-  })
-}
+export default () => lib.callback.register('doesUserAlreadyExist', async (source: string) => doesUserAlreadyExist(source))

d510b3dd35b10a02d036d3651bef68f18ae304fd
tripplerscripts <tripplerscripts@github.com>
2025-12-06 03:28:45 -0800

    feat(callbacks): add getOutgoingFriends API callback
    - Implement getOutgoingFriends function to fetch outgoing friends asynchronously
    - Use lib.callback.await with optional timeout parameter
    - Return the response from the callback API function

---
+++ backend/client/callbacks/APIs/getOutgoingFriends.ts
@@ -0,0 +1,6 @@
+import lib from '../../lib'
+
+export default async function getOutgoingFriends(timeout?: number): Promise<typeof response> {
+  const response = await lib.callback.await('getOutgoingFriends', timeout)
+  return response
+}

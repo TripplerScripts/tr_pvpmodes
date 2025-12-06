edd6c9ae023edbdb9b4a8dd40c58ab018b9c98cd
tripplerscripts <tripplerscripts@github.com>
2025-12-06 03:29:38 -0800

    feat(callbacks): add sendUserFriendInvitation API function
    - Introduce sendUserFriendInvitation to backend client callbacks
    - Use lib.callback.await with provided timeout and name parameters
    - Return the response from the callback invocation

---
+++ backend/client/callbacks/APIs/sendUserFriendInvitation.ts
@@ -0,0 +1,6 @@
+import lib from '../../lib'
+
+export default async function sendUserFriendInvitation(timeout: number, name: string): Promise<typeof response> {
+  const response = await lib.callback.await('sendUserFriendInvitation', timeout, name)
+  return response
+}

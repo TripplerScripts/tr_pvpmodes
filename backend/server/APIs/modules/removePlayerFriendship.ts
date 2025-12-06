4977c63f61590e4169c00fc80fc8e2ec5f14ef5e
LenixDev <149199196+LenixDev@users.noreply.github.com>
2025-12-06 22:33:33 +0100

    refactor(back-end): simplify removePlayerFriendship callback registration
    - Replace inline license retrieval with utility function getPlayerLicense
    - Remove unnecessary wrapper function and directly export callback registration
    - Maintain existing logic of removing friendship entries from sender and receiver
    - Keep database update calls for both users consistent and atomic

--- backend/server/APIs/modules/removePlayerFriendship.ts
+++ backend/server/APIs/modules/removePlayerFriendship.ts
@@ -1,22 +1,21 @@
 import lib from '../../lib'
 import { getSingleRow, updateSingleColumn } from '../../database'
+import getPlayerLicense from '../../utils/getPlayerLicense'
 
-export default (): void => {
-  lib.callback.register('removePlayerFriendship', async (source: string, name: string) => {
-    const license = GetPlayerIdentifierByType(source, 'license')
-    const senderResponse = await getSingleRow(['name', 'friends'], 'tr_competitive_users', 'license', license)
-    if (!senderResponse) return
-    const receiverResponse = await getSingleRow(['friends'], 'tr_competitive_users', 'name', name)
-    if (!receiverResponse) return
+export default () => lib.callback.register('removePlayerFriendship', async (source: string, name: string) => {
+  const license = getPlayerLicense(source)
+  const senderResponse = await getSingleRow(['name', 'friends'], 'tr_competitive_users', 'license', license)
+  if (!senderResponse) return
+  const receiverResponse = await getSingleRow(['friends'], 'tr_competitive_users', 'name', name)
+  if (!receiverResponse) return
 
-    const senderName = senderResponse.name
-    const senderRequests = JSON.parse(senderResponse.friends)
-    const receiverRequests = JSON.parse(receiverResponse.friends)
-    senderRequests.splice(senderRequests.indexOf(name), 1)
-    receiverRequests.splice(receiverRequests.indexOf(senderName), 1)
+  const senderName = senderResponse.name
+  const senderRequests = JSON.parse(senderResponse.friends)
+  const receiverRequests = JSON.parse(receiverResponse.friends)
+  senderRequests.splice(senderRequests.indexOf(name), 1)
+  receiverRequests.splice(receiverRequests.indexOf(senderName), 1)
 
-    const senderAffectedColumn = updateSingleColumn('tr_competitive_users', 'friends', 'license', JSON.stringify(senderRequests), license)
-    const receiverAffectedColumn = updateSingleColumn('tr_competitive_users', 'friends', 'name', JSON.stringify(receiverRequests), name)
-    return senderAffectedColumn && receiverAffectedColumn
-  })
-}
+  const senderAffectedColumn = updateSingleColumn('tr_competitive_users', 'friends', 'license', JSON.stringify(senderRequests), license)
+  const receiverAffectedColumn = updateSingleColumn('tr_competitive_users', 'friends', 'name', JSON.stringify(receiverRequests), name)
+  return senderAffectedColumn && receiverAffectedColumn
+})

38792aaafab7030d604fcc698eb919fbe5ac141b
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-07 04:13:43 -0800

    refactor(server): update removeIncomingRequest to use userId and updateRow
    - Change parameter from name to userId for clearer identification
    - Modify database queries to use 'userId' instead of 'name' or 'license' fields
    - Replace updateSingleColumn with updateRow for updating invitation lists
    - Adjust indices removal logic to use userId consistently
    - Improve SQL query syntax by using parameterized condition strings

--- backend/server/APIs/modules/removeIncomingRequest.ts
+++ backend/server/APIs/modules/removeIncomingRequest.ts
@@ -1,21 +1,21 @@
 import lib from '../../lib'
-import { getSingleRow, updateSingleColumn } from '../../database'
+import { getSingleRow, updateRow } from '../../database'
 import getPlayerLicense from '../../utils/getPlayerLicense'
 
-export default () => lib.callback.register('removeIncomingRequest', async (source: string, name: string) => {
+export default () => lib.callback.register('removeIncomingRequest', async (source: string, userId: string) => {
   const license = getPlayerLicense(source)
-  const senderResponse = await getSingleRow(['name', 'incomingInvitations'], 'tr_competitive_users', 'license', license)
+  const senderResponse = await getSingleRow('name, incomingInvitations', 'tr_competitive_users', 'license = ?', license)
   if (!senderResponse) return
-  const receiverResponse = await getSingleRow(['outgoingInvitations'], 'tr_competitive_users', 'name', name)
+  const receiverResponse = await getSingleRow('outgoingInvitations', 'tr_competitive_users', 'userId = ?', userId)
   if (!receiverResponse) return
 
-  const senderName = senderResponse.name
+  const senderUserId = senderResponse.userId
   const senderRequests = JSON.parse(senderResponse.incomingInvitations)
   const receiverRequests = JSON.parse(receiverResponse.outgoingInvitations)
-  senderRequests.splice(senderRequests.indexOf(name), 1)
-  receiverRequests.splice(receiverRequests.indexOf(senderName), 1)
+  senderRequests.splice(senderRequests.indexOf(userId), 1)
+  receiverRequests.splice(receiverRequests.indexOf(senderUserId), 1)
 
-  const senderAffectedColumn = updateSingleColumn('tr_competitive_users', 'incomingInvitations', 'license', JSON.stringify(senderRequests), license)
-  const receiverAffectedColumn = updateSingleColumn('tr_competitive_users', 'outgoingInvitations', 'name', JSON.stringify(receiverRequests), name)
+  const senderAffectedColumn = updateRow('tr_competitive_users', 'incomingInvitations', 'license = ?', JSON.stringify(senderRequests), license)
+  const receiverAffectedColumn = updateRow('tr_competitive_users', 'outgoingInvitations', 'userId = ?', JSON.stringify(receiverRequests), userId)
   return senderAffectedColumn && receiverAffectedColumn
 })

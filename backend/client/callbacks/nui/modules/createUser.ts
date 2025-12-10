f697ee77bfec0b0d1a25f6c33decf5961856a350
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-10 14:07:36 -0800

    fix(backend): add logging for user creation callback
    - Added console.log to output data received in createUser callback
    - Wrapped callback invocation in curly braces for clarity

--- backend/client/callbacks/nui/modules/createUser.ts
+++ backend/client/callbacks/nui/modules/createUser.ts
@@ -1,5 +1,6 @@
 import createUser from '../../APIs/createUser'
 
-export default (): void => RegisterNuiCallback('createUser', async (data: { name: string, avatar: string }, callback: (response: any) => void) =>
-  callback(await createUser(null, data[0], data[1]))
+export default (): void => RegisterNuiCallback('createUser', async (data: { name: string, avatar: string }, callback: (response: any) => void) =>{
+  console.log(data)
+  callback(await createUser(null, data[0], data[1]))}
 )

afc1db6b37b5ec77ade5e08b00a7e81fbbf34711
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-10 00:48:51 -0800

    fix(createUser): add avatar parameter to user creation callback
    - Updated callback to accept avatar along with source and name
    - Passed avatar parameter to createUserIntoTheDatabase function
    - Enabled user creation with custom avatar support

--- backend/server/APIs/modules/createUser.ts
+++ backend/server/APIs/modules/createUser.ts
@@ -2,4 +2,4 @@ import lib from '../../lib'
 import createUserIntoTheDatabase from '../../modules/createUserIntoTheDatabase'
 import getPlayerLicense from '../../utils/getPlayerLicense'
 
-export default () => lib.callback.register('createUser', (source: string, name: string) => createUserIntoTheDatabase(getPlayerLicense(source), name))
+export default () => lib.callback.register('createUser', (source: string, name: string, avatar: string) => createUserIntoTheDatabase(getPlayerLicense(source), name, avatar))

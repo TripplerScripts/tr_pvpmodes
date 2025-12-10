6b4142b9b49918b3197fa1d0005fe7e813b9ba50
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-09 23:25:19 -0800

    feat(playerDetails): add singleton pattern and async user profile fetching
    - Introduce a static instance for singleton implementation
    - Initialize player name asynchronously using getUserProfile API
    - Add getUserDetails method to fetch and cache user profile by identity
    - Implement refresh method to update player name with latest profile data
    - Set default avatar URLs and initialize name as empty string in constructor

--- frontend/src/modules/playerDetails.ts
+++ frontend/src/modules/playerDetails.ts
@@ -1,11 +1,26 @@
-
+import getUserProfile from '../APIs/getUserProfile'
 
 export default class {
-
+  private static instance: any = null
+  
   avatar: string = 'https://r2.fivemanage.com/COKMc8Wcmk9K5dp547rEw/Lenix_924.png'
   level: number = 7
-  name: string = 'Lenix'
+  name: string = ''
   incomingAvatar: string = 'https://i.pravatar.cc/300'
   outgoingAvatar: string = 'https://i.pravatar.cc/300'
   friendAvatar: string = 'https://i.pravatar.cc/300'
+
+  public constructor() {}
+
+  static async getUserDetails(identity: number) {
+    if (!this.instance) {
+      this.instance = new this()
+      this.instance.name = await getUserProfile(identity).then(response => response.name)
+    }
+    return this.instance
+  }
+
+  async refresh(identity: number) {
+    this.name = await getUserProfile(identity)
+  }
 }

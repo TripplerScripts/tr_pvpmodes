8269df808a3f174cb69188cfa5562ad8b33f59b3
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-10 14:07:57 -0800

    fix(getUserProfile): include avatar field in user profile query
    - Updated database query to select both name and avatar fields
    - Modified getUserProfile module to return additional avatar data
    - Ensured compatibility with existing callback registration system

--- backend/server/APIs/modules/getUserProfile.ts
+++ backend/server/APIs/modules/getUserProfile.ts
@@ -1,4 +1,4 @@
 import lib from '../../lib'
 import { getSingleRow } from "../../database";
 
-export default () => lib.callback.register('getUserProfile', (_source: string, identity: number) => getSingleRow('name', 'tr_competitive_users', 'identity = ?', identity))
+export default () => lib.callback.register('getUserProfile', (_source: string, identity: number) => getSingleRow('name, avatar', 'tr_competitive_users', 'identity = ?', identity))

cf6d40d88b4ca840046c3e67deb84cb0609b96eb
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-11 10:51:00 -0800

    fix(client): remove debug log from getUserProfile callback
    - Removed console.log from getUserProfile Nui callback implementation
    
    fix(server): return only identity in doesUserExist module
    
    - Modified doesUserExist to return identity field instead of full response object

--- backend/client/callbacks/nui/modules/getUserProfile.ts
+++ backend/client/callbacks/nui/modules/getUserProfile.ts
@@ -2,6 +2,5 @@ import getUserProfile from '../../APIs/getUserProfile'
 
 export default (): void => RegisterNuiCallback('getUserProfile', async (data: Array<any>, callback: (response: any) => void) => {
   const response = await getUserProfile(null, data[0])
-  console.log(response)
   callback(response)
 })
--- backend/server/modules/doesUserExist.ts
+++ backend/server/modules/doesUserExist.ts
@@ -3,5 +3,5 @@ import getPlayerLicense from '../utils/getPlayerLicense'
 
 export default async (source: string) => {
   const response = await getSingleRow('identity', 'tr_competitive_users', 'license = ?', getPlayerLicense(source))
-  return response
+  return response.identity
 }

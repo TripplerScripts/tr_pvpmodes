f66a8dfda35107e531a4f8beff71b8791f228b66
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-12 08:27:49 -0800

    fix(client): simplify parameter typing in removeIncomingRequest callback
    - Removed explicit type annotation from data parameter for cleaner code
    - Kept callback function signature unchanged
    - Maintained async behavior and response handling

--- backend/client/callbacks/nui/modules/removeIncomingRequest.ts
+++ backend/client/callbacks/nui/modules/removeIncomingRequest.ts
@@ -1,5 +1,5 @@
 import removeIncomingRequest from '../APIs/removeIncomingRequest'
 
-export default () => RegisterNuiCallback('removeIncomingRequest', async (data: Array<any>, callback: (response: any) => void) =>
+export default () => RegisterNuiCallback('removeIncomingRequest', async (data, callback) =>
   callback(await removeIncomingRequest(null, data[0]))
 )

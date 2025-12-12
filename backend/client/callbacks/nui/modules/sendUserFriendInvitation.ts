bd763170c5020b725bc44938e84646ca9848e6b2
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-12 08:28:02 -0800

    refactor(client): simplify sendUserFriendInvitation callback parameters
    - Removed explicit type annotation for data parameter in callback
    - Cleaned up arrow function formatting for better readability
    - Maintained existing functionality of sending user friend invitation

--- backend/client/callbacks/nui/modules/sendUserFriendInvitation.ts
+++ backend/client/callbacks/nui/modules/sendUserFriendInvitation.ts
@@ -1,5 +1,5 @@
 import sendUserFriendInvitation from '../APIs/sendUserFriendInvitation'
 
-export default () => RegisterNuiCallback('sendUserFriendInvitation', async (data: Array<string>, callback: (response: any) => void) =>
+export default () => RegisterNuiCallback('sendUserFriendInvitation', async (data, callback) =>
   callback(await sendUserFriendInvitation(null, data[0]))
 )

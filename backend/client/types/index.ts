348b83043bbc47699e96f02dc851b132411f44d5
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-12 08:29:54 -0800

    feat(types): add RegisterNuiCallback function declaration
    - Declare global function RegisterNuiCallback with event and callback parameters
    - Callback supports data array and response callback function
    - Enhance typing for Nui callback registration in global scope

--- backend/client/types/index.ts
+++ backend/client/types/index.ts
@@ -1,6 +1,7 @@
 declare global {
   var lastCoords: number[];
   var camHandle: number;
+  function RegisterNuiCallback(event: string, callback: (data: any[], callback: (response: any) => void) => void): void;
 }
 
 export {}

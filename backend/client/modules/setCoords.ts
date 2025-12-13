7b00c596800471ca06a30872dece0e802f7a6ff1
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-13 04:43:56 -0800

    refactor(client): remove setCoords module
    - Deleted the entire setCoords function from the client module
    - Removed redundant coordinate setting logic for PlayerPedId
    - Cleaned up unused code related to entity position and heading adjustments

--- backend/client/modules/setCoords.ts
+++
@@ -1,4 +0,0 @@
-export default (coords: number[]) => {
-  SetEntityCoords(PlayerPedId(), coords[0], coords[1], coords[2], true, false, true, false)
-  if (coords[3] !== undefined) SetEntityHeading(PlayerPedId(), coords[3])
-}

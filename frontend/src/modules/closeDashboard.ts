ffe23861c80b2639abca5e296a3f4ace1f603254
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-13 04:46:35 -0800

    refactor(closeDashboard): remove unused closeDashboard module
    - Deleted the closeDashboard.ts file as it is no longer used
    - Removed sendCallback invocation from closeDashboard
    - Eliminated hiding of root element in the dashboard close logic

--- frontend/src/modules/closeDashboard.ts
+++
@@ -1,9 +0,0 @@
-import sendCallback from "../components/sendCallback"
-
-export default (): void => {
-  sendCallback<boolean>('unfocus')
-  const root = document.getElementById('root')
-  if (root) {
-    root.classList.add('hidden')
-  }
-}

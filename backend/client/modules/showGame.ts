6c7efc0aa3094d3b0fa4902aad8e0ccb70bb516d
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-14 09:52:16 -0800

    feat(showGame): add function to show game UI with camera setup
    - Import nuiMessage component for UI interaction
    - Trigger nuiMessage with action to show interface
    - Set NUI focus to true for input capture
    - Call createCam function to set up camera at specific coordinates

---
+++ backend/client/modules/showGame.ts
@@ -0,0 +1,9 @@
+import nuiMessage from "../components/nuiMessage"
+
+export default () => {
+  nuiMessage({
+    action: 'show'
+  })
+  SetNuiFocus(true, true)
+  exports.tr_competitive.createCam(200, 200, 2000)
+}

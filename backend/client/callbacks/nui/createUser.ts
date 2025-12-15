a2bf334172cf661fa6a1911dba2792a6db09d6ff
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-15 02:04:53 -0800

    refactor(client): simplify createUser callback syntax
    - Removed unnecessary curly braces and return statement
    - Converted callback arrow function to concise body form
    - Improved readability by reducing line clutter

--- backend/client/callbacks/nui/createUser.ts
+++ backend/client/callbacks/nui/createUser.ts
@@ -1,5 +1,5 @@
 import createUser from '../../APIs/callbacks/createUser'
 
-export default () => RegisterNuiCallback('createUser', async (data, callback) =>{
-  callback(await createUser(undefined, data[0], data[1]))}
+export default () => RegisterNuiCallback('createUser', async (data, callback) =>
+  callback(await createUser(undefined, data[0], data[1]))
 )

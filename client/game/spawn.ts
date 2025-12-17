bd02a7d4b00d50772422ef015211d8bcf3e2b65d
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-17 00:15:23 -0800

    fix(client): update spawn and logout to use onboarding module
    - Changed spawn function to call onboarding's startCharacterProcess instead of spawn module
    - Updated resource stop event to emit logout event for onboarding server namespace
    - Replaced references from "tr_spawn" to "tr_onboarding" in client code

--- client/game/spawn.ts
+++ client/game/spawn.ts
@@ -1,3 +1,3 @@
 import { showGame, hideGame } from "."
 
-export default async () => exports.tr_spawn.startCharacterProcess([476.8345, -1314.8319, 28.2045, 239.2849], [476.8345, -1314.8319, 28.2045, 239.2849], hideGame, showGame)
+export default async () => exports.tr_onboarding.startCharacterProcess([476.8345, -1314.8319, 28.2045, 239.2849], [476.8345, -1314.8319, 28.2045, 239.2849], hideGame, showGame)
--- client/index.ts
+++ client/index.ts
@@ -3,7 +3,7 @@ import './exports'
 
 on('onResourceStop', (resourceName: string) => {
   if (resourceName === GetCurrentResourceName()) {
-    emitNet('tr_spawn/server/logout')
+    emitNet('tr_onboarding/server/logout')
   }
 })
 

740b84bedf382d55e38aba955f117ed74a255923
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-20 14:47:30 -0800

    fix(friendship): remove unnecessary undefined arguments in callbacks
    - Updated acceptFriendship callback to remove undefined parameter
    - Updated cancelOutgoingFriendship callback to remove undefined parameter
    - Updated removeIncomingRequest callback to remove undefined parameter
    - Updated removePlayerFriendship callback to remove undefined parameter
    - Updated sendUserFriendInvitation callback to remove undefined parameter

--- client/nui/friendship.ts
+++ client/nui/friendship.ts
@@ -10,12 +10,12 @@ import {
   sendUserFriendInvitation
  } from '../api/friendship'
 
-RegisterNuiCallback('acceptFriendship', async (data, callback) => callback(await acceptFriendship(undefined, data[0])))
-RegisterNuiCallback('cancelOutgoingFriendship', async (data, callback) => callback(await cancelOutgoingFriendship(undefined, data[0])))
+RegisterNuiCallback('acceptFriendship', async (data, callback) => callback(await acceptFriendship(data[0])))
+RegisterNuiCallback('cancelOutgoingFriendship', async (data, callback) => callback(await cancelOutgoingFriendship(data[0])))
 RegisterNuiCallback('getFriendablePlayers', async (_data, callback) => callback(await getFriendablePlayers()))
 RegisterNuiCallback('getIncomingFriends', async (_data, callback) => callback(await getIncomingFriends()))
 RegisterNuiCallback('getOutgoingFriends', async (_data, callback) => callback(await getOutgoingFriends()))
 RegisterNuiCallback('getPlayerFriends', async (_data, callback) => callback(await getPlayerFriends()))
-RegisterNuiCallback('removeIncomingRequest', async (data, callback) => callback(await removeIncomingRequest(undefined, data[0])))
-RegisterNuiCallback('removePlayerFriendship', async (data, callback) => callback(await removePlayerFriendship(undefined, data[0])))
-RegisterNuiCallback('sendUserFriendInvitation', async (data, callback) => callback(await sendUserFriendInvitation(undefined, data[0])))
+RegisterNuiCallback('removeIncomingRequest', async (data, callback) => callback(await removeIncomingRequest(data[0])))
+RegisterNuiCallback('removePlayerFriendship', async (data, callback) => callback(await removePlayerFriendship(data[0])))
+RegisterNuiCallback('sendUserFriendInvitation', async (data, callback) => callback(await sendUserFriendInvitation(data[0])))

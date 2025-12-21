9f6f287cf7e92ea4d542d617037a8d6671a69510
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-21 00:35:45 -0800

    refactor(api): replace sendCallback with triggerNuiCallback for friendship API
    - Remove direct import of sendCallback from UI components
    - Use triggerNuiCallback from '@trippler/tr_lib/web' for all friendship functions
    - Update functions to pass parameters as objects for consistency
    - Maintain existing logic for handling responses and refreshing UI elements

--- web/src/api/friendship.ts
+++ web/src/api/friendship.ts
@@ -1,4 +1,3 @@
-import { sendCallback } from "@lenixdev/ui_components"
 import refreshFriends from "../utils/dom"
 import createFriendItem from "../modules/createFriendItem"
 import {
@@ -12,22 +11,23 @@ import {
   RemovePlayerFriendship,
   SendUserFriendInvitation,
 } from "../../../shared/types/callback"
+import { triggerNuiCallback } from '@trippler/tr_lib/web'
 
 export const acceptFriendship = async (identity: number) => {
-  const response = await sendCallback<AcceptFriendship>('acceptFriendship', identity)
+  const response = await triggerNuiCallback<AcceptFriendship>('acceptFriendship', {identity})
   if (!response) return
   createFriendItem(identity)
   return true
 }
 export const getPlayerFriends = async () => {
-  const friends = await sendCallback<GetPlayerFriends>('getPlayerFriends')
+  const friends = await triggerNuiCallback<GetPlayerFriends>('getPlayerFriends')
   refreshFriends((friends as number[])?.length)
   return friends
 }
-export const getOutgoingFriends = () => sendCallback<GetOutgoingFriends>('getOutgoingFriends')
-export const getIncomingFriends = () => sendCallback<GetIncomingFriends>('getIncomingFriends')
-export const getFriendablePlayers = () => sendCallback<GetFriendablePlayers>('getFriendablePlayers')
-export const removeIncomingRequest = (identity: number) => sendCallback<RemoveIncomingRequest>('removeIncomingRequest', identity)
-export const removePlayerFriendship = (identity: number) => sendCallback<RemovePlayerFriendship>('removePlayerFriendship', identity)
-export const sendUserFriendInvitation = (identity: number) => sendCallback<SendUserFriendInvitation>('sendUserFriendInvitation', identity)
-export const cancelOutgoingFriendship = (identity: number) => sendCallback<CancelOutgoingFriendship>('cancelOutgoingFriendship', identity)
+export const getOutgoingFriends = () => triggerNuiCallback<GetOutgoingFriends>('getOutgoingFriends')
+export const getIncomingFriends = () => triggerNuiCallback<GetIncomingFriends>('getIncomingFriends')
+export const getFriendablePlayers = () => triggerNuiCallback<GetFriendablePlayers>('getFriendablePlayers')
+export const removeIncomingRequest = (identity: number) => triggerNuiCallback<RemoveIncomingRequest>('removeIncomingRequest', { identity })
+export const removePlayerFriendship = (identity: number) => triggerNuiCallback<RemovePlayerFriendship>('removePlayerFriendship', { identity })
+export const sendUserFriendInvitation = (identity: number) => triggerNuiCallback<SendUserFriendInvitation>('sendUserFriendInvitation', { identity })
+export const cancelOutgoingFriendship = (identity: number) => triggerNuiCallback<CancelOutgoingFriendship>('cancelOutgoingFriendship', { identity })

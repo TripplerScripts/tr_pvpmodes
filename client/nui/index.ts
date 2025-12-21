2262e5683dd99b7a2a248dc66cb47045547e6690
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-21 00:35:19 -0800

    refactor(client): replace RegisterNuiCallback with onNuiCallback usage
    - Imported onNuiCallback from '@trippler/tr_lib/client'
    - Replaced RegisterNuiCallback calls with onNuiCallback for 'closeGame'
    - Updated callback implementations to use onNuiCallback
    - Simplified asynchronous callback handling in startCharacterProcess
    - Streamlined getServerProfile callback invocation

--- client/nui/index.ts
+++ client/nui/index.ts
@@ -3,9 +3,8 @@ import './user'
 import { closeGame } from '../game'
 import { profile } from '../../shared/constants/config'
 import startCharacterProcess from '../game/index'
+import { onNuiCallback } from '@trippler/tr_lib/client'
 
-RegisterNuiCallback('closeGame', (_data, callback) => callback(closeGame()))
-RegisterNuiCallback('startCharacterProcess', async (_data, callback: Function) => callback(await startCharacterProcess()))
-RegisterNuiCallback('getServerProfile', async (_data, callback) => {
-  callback(profile)
-})
+onNuiCallback('closeGame', (_data, callback) => callback(closeGame()))
+onNuiCallback('startCharacterProcess', async (_data, callback) => callback(await startCharacterProcess()))
+onNuiCallback('getServerProfile', async (_data, callback) => callback(profile))

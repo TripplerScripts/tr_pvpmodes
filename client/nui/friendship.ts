e2881935760b5b7ad468eb25001fd0b0ce0854b8
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-21 00:35:12 -0800

    refactor(friendship): replace RegisterNuiCallback with onNuiCallback for handling callbacks
    - Changed callback registration to use onNuiCallback from '@trippler/tr_lib/client'
    - Added typing for callback data (e.g., { identity: number } or null)
    - Updated callback parameter access from array indices to named properties
    - Improved code readability and consistency in friendship.ts client code

--- client/nui/friendship.ts
+++ client/nui/friendship.ts
@@ -1,3 +1,4 @@
+import { onNuiCallback } from '@trippler/tr_lib/client'
 import {
   acceptFriendship,
   cancelOutgoingFriendship,
@@ -8,14 +9,14 @@ import {
   removeIncomingRequest,
   removePlayerFriendship,
   sendUserFriendInvitation
- } from '../api/friendship'
+} from '../api/friendship'
 
-RegisterNuiCallback('acceptFriendship', async (data, callback) => callback(await acceptFriendship(data[0])))
-RegisterNuiCallback('cancelOutgoingFriendship', async (data, callback) => callback(await cancelOutgoingFriendship(data[0])))
-RegisterNuiCallback('getFriendablePlayers', async (_data, callback) => callback(await getFriendablePlayers()))
-RegisterNuiCallback('getIncomingFriends', async (_data, callback) => callback(await getIncomingFriends()))
-RegisterNuiCallback('getOutgoingFriends', async (_data, callback) => callback(await getOutgoingFriends()))
-RegisterNuiCallback('getPlayerFriends', async (_data, callback) => callback(await getPlayerFriends()))
-RegisterNuiCallback('removeIncomingRequest', async (data, callback) => callback(await removeIncomingRequest(data[0])))
-RegisterNuiCallback('removePlayerFriendship', async (data, callback) => callback(await removePlayerFriendship(data[0])))
-RegisterNuiCallback('sendUserFriendInvitation', async (data, callback) => callback(await sendUserFriendInvitation(data[0])))
+onNuiCallback<{ identity: number }>('acceptFriendship', async (data, callback) => callback(await acceptFriendship(data.identity)))
+onNuiCallback<{ identity: number }>('cancelOutgoingFriendship', async (data, callback) => callback(await cancelOutgoingFriendship(data.identity)))
+onNuiCallback<{ identity: number }>('removeIncomingRequest', async (data, callback) => callback(await removeIncomingRequest(data.identity)))
+onNuiCallback<{ identity: number }>('removePlayerFriendship', async (data, callback) => callback(await removePlayerFriendship(data.identity)))
+onNuiCallback<{ identity: number }>('sendUserFriendInvitation', async (data, callback) => callback(await sendUserFriendInvitation(data.identity)))
+onNuiCallback<null>('getPlayerFriends', async (_data, callback) => callback(await getPlayerFriends()))
+onNuiCallback<null>('getIncomingFriends', async (_data, callback) => callback(await getIncomingFriends()))
+onNuiCallback<null>('getOutgoingFriends', async (_data, callback) => callback(await getOutgoingFriends()))
+onNuiCallback<null>('getFriendablePlayers', async (_data, callback) => callback(await getFriendablePlayers()))

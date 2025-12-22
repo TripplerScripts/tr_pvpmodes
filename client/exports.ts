6e3554dee8a95aa098cfddd3fe88d6b6ab53adb6
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-22 13:58:06 -0800

    fix(client): use predefined spawn coordinates for character process
    - Import creationSpawnCoords and defaultSpawnCoords from shared constants
    - Replace hardcoded spawn coordinates with imported constants
    - Update startCharacterProcess call to use new coordinates variables

--- client/exports.ts
+++ client/exports.ts
@@ -1,3 +1,5 @@
+import { creationSpawnCoords, defaultSpawnCoords } from '../shared/constants'
+
 globalThis.exports('start', async () => {
-  exports.tr_onboarding.startCharacterProcess([298.5893, -581.2991, 43.260, 111.2940], [298.5893, -581.2991, 43.260, 111.2940])
+  globalThis.exports.tr_onboarding.startCharacterProcess(creationSpawnCoords, defaultSpawnCoords)
 })

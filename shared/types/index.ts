b313677e98fcada8bf6ab32abe7f1c246b2e05b7
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-26 13:01:16 -0800

    refactor(types): update Suggestion interface and add registeredCommands
    - Changed Suggestion.params to an array of objects with detailed fields
    - Modified property types in Suggestion and Message interfaces
    - Added registeredCommands object with predefined command keys and parameters
    - Defined CommandName type based on Suggestion name property

--- shared/types/index.ts
+++ shared/types/index.ts
@@ -1,27 +1,43 @@
 export interface Suggestion {
-  name: string;
-  help: string;
-  params: string[];
+  name: string
+  help: string | undefined
+  params: {
+    name: string
+    help: string | undefined
+    optional: boolean | undefined
+    type: `playerId` | `string` | `number`
+  }[]
 }
 
 export interface Message {
-  args: string[];
-  template: string;
-  params?: { [key: string]: string };
-  multiline?: boolean;
-  color?: [ number, number, number ];
-  templateId?: number;
-  mode?: string;
-  modeData?: Mode;
+  args: string[]
+  template: string
+  params?: { [key: string]: string }
+  multiline?: boolean
+  color?: [ number, number, number ]
+  templateId?: number
+  mode?: string
+  modeData?: Mode
 
-  id?: string;
+  id?: string
 }
 
 export interface Mode {
-  name: string;
-  displayName: string;
-  color: string;
-  hidden?: boolean;
-  isChannel?: boolean;
-  isGlobal?: boolean;
+  name: string
+  displayName: string
+  color: string
+  hidden?: boolean
+  isChannel?: boolean
+  isGlobal?: boolean
 }
+
+export const registeredCommands = {
+  setjob: ['id', 'job', 'rank',],
+  vector3: ['id', 'job', 'rank'],
+  vector4: ['id', 'job', 'rank'],
+  vec2: ['id', 'job', 'rank'],
+  vec222: ['id', 'job', 'rank'],
+  tx: ['id', 'job', 'rank'],
+}
+
+export type CommandName = Suggestion['name']

c3ec53d8917bd146278ba9910473d9a6812d2085
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-26 13:01:01 -0800

    fix(server): replace StopResource with ExecuteCommand to stop chat
    - Changed stopping of 'chat' resource from StopResource to ExecuteCommand
    - Used command string 'stop chat' instead of direct function call
    - Improved consistency with command execution method

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { fatal, info, trace } from '@trippler/tr_lib/shared'
 setTimeout(() => {
   if (GetResourceState('chat') !== 'missing') {
     info("the resource 'chat' was detected, stopping 'chat'...")
-    StopResource('chat')
+    ExecuteCommand('stop chat')
   }
 })
 

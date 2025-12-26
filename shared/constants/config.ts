e31c8ed66aa4aa432f24c3ddd213560ecd8ffff2
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-26 13:01:09 -0800

    refactor(constants): replace registeredCommands with recentCommands array
    - Removed the registeredCommands object containing command metadata
    - Imported CommandName type from external types module
    - Added recentCommands array with 'setjob' and 'vector3' commands
    - Introduced a suggestionsCount constant set to 5 for limiting suggestions

--- shared/constants/config.ts
+++ shared/constants/config.ts
@@ -1,11 +1,4 @@
-export const registeredCommands = {
-  setjob: ['id', 'job', 'rank',],
-  vector3: ['id', 'job', 'rank'],
-  vector4: ['id', 'job', 'rank'],
-  vec2: ['id', 'job', 'rank'],
-  vec222: ['id', 'job', 'rank'],
-  tx: ['id', 'job', 'rank'],
-}
-export type CommandName = keyof typeof registeredCommands
+import { CommandName } from "../types"
+
 export const recentCommands: CommandName[] = ['setjob', 'vector3']
 export const suggestionsCount = 5

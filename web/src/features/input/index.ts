59421269588bb6d53030ff04e6c888e805b82faf
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-27 14:48:53 -0800

    refactor(input): replace UI components with hooks and improve command clearing
    - Replace Input and Button components with useInput and useButton hooks
    - Change button icon from times to trash and update styles for hover effect
    - Add useClearCommandSelection call on button click to clear command selection
    - Clear input value after clearing frequently used commands
    - Update import statements to include new hooks from @trippler/tr_kit/web and local hooks

--- web/src/features/input/index.ts
+++ web/src/features/input/index.ts
@@ -1,10 +1,12 @@
-import { Button, createElement, Input } from '@lenixdev/ui_components'
+import { createElement, Input } from '@lenixdev/ui_components'
+import { useButton, useInput } from '@trippler/tr_kit/web'
 import useUpdateSuggetions from '../../hooks/useUpdateSuggetions'
 import { suggestionsCount } from '../../../../shared/constants/config'
 import { useChangeBorderColor } from '../../hooks/useChangeBorderColor'
 import { passCommand } from '../../api'
 import { getPassedArgumentsFirstString } from '../../utils'
 import { clearStoredFrequentlyUsedCommands, getStoredFrequentlyUsedCommands, useStoreFrequentlyUsedCommands } from '../../hooks/useFrequentlyUsedCommands'
+import { useClearCommandSelection } from '../../hooks/useCommandSelection'
 
 createElement({
   parent: `chat-container`,
@@ -12,7 +14,7 @@ createElement({
   className: `w-full h-[40%] bg-stone-600 flex flex-col gap-1`
 })
 
-export const input = Input({
+export const input = useInput({
   parent: `chat-input`,
   placeholder: `type / to start a command`,
   className: `input w-full bg-stone-700 p-2 outline-none text-white transition-all`,
@@ -34,12 +36,14 @@ createElement({
   className: `relative w-full h-full bg-inherit flex flex-col justify-between`,
 })
 
-Button({
+useButton({
   parent: `chat-suggestions-items`,
-  content: '<i class="fas fa-times"></i>',
-  className: `absolute top-0 right-0 z-10 w-8 h-8 bg-red-500`, // Fixed size, added z-index
+  content: '<i class="fas fa-trash"></i>',
+  className: `absolute top-0 right-0 text-stone-400 z-10 hover:text-stone-300 mx-2`,
   onClick: () => {
     clearStoredFrequentlyUsedCommands()
+    useClearCommandSelection()
+    input.value = ``
   }
 })
 

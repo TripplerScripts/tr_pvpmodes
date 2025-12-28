c10e70d269b20529a60c31baf92f21a0258580b5
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-28 03:09:23 -0800

    fix(constants): update defaultSpawnCoords export for web compatibility
    - Change defaultSpawnCoords to dynamically obtain last position if available
    - Fallback to creationSpawnCoords when dynamic position is not available
    - Add comment explaining the reason for the modification related to web imports

--- shared/constants/index.ts
+++ shared/constants/index.ts
@@ -9,4 +9,5 @@ export const maxTextLength = 128
 export const messagesCooldown = 5
 export const messagesFadeDuration = 5000
 export const creationSpawnCoords = [298.5893, -581.2991, 43.260, 111.2940]
-export const defaultSpawnCoords = [298.5893, -581.2991, 43.260, 111.2940]
+// somehow the web is importing this shit
+export const defaultSpawnCoords = (typeof exports !== 'undefined' && exports?.tr_freeroam?.getLastPosition?.()) ?? creationSpawnCoords

484fafa730505eb472f0f6942e1f9f5f371e77ad
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-01-03 03:54:55 -0800

    fix(elements): update background image URL in main menu container
    - Replace local bg.jpg with external image URL for onboarding background
    - Remove dependence on local background image assets
    - Update CSS class for main menu container to use new background image link

--- web/src/elements/index.ts
+++ web/src/elements/index.ts
@@ -10,7 +10,7 @@ useDiv({
 useDiv({
   parent: "root",
   id: "main-menu",
-  className: "w-[100vw] h-[100vh] flex items-end justify-center py-52 bg-stone-900/20 bg-[url(./images/bg.jpg)] bg-cover bg-center"
+  className: "w-[100vw] h-[100vh] flex items-end justify-center py-52 bg-stone-900/20 bg-[url(https://r2.fivemanage.com/COKMc8Wcmk9K5dp547rEw/onboardingbg2.jpg)] bg-cover bg-center"
 })
 
 useDiv({
